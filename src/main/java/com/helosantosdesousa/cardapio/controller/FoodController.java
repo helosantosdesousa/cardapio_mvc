7ddde4053d9de0c388f65dcafda43b842f1711ed
helosantosdesousa <ssheloly@gmail.com>
2025-09-02 18:54:46 -0300

    add h2 database

--- src/main/java/com/helosantosdesousa/cardapio/controller/FoodController.java
+++ src/main/java/com/helosantosdesousa/cardapio/controller/FoodController.java
@@ -1,4 +1,16 @@
 package com.helosantosdesousa.cardapio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("food")
+//toda vez que o endpoint for 'food' ele manda pra cá
 public class FoodController {
+
+    @GetMapping
+    public void getAll(){
+
+    }
 }
