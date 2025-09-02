6561494900829d798bb3f372b7bd189e527fe514
helosantosdesousa <ssheloly@gmail.com>
2025-09-02 19:55:11 -0300

    mudança de war para jar

--- src/main/java/com/helosantosdesousa/cardapio/controller/FoodController.java
+++ src/main/java/com/helosantosdesousa/cardapio/controller/FoodController.java
@@ -1,16 +1,25 @@
 package com.helosantosdesousa.cardapio.controller;
 
+import com.helosantosdesousa.cardapio.entities.food.Food;
+import com.helosantosdesousa.cardapio.entities.food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 //toda vez que o endpoint for 'food' ele manda pra cá
 public class FoodController {
+   @Autowired //usa ao inves de instanciar um objeto foodRepository
+    private FoodRepository repository;
 
     @GetMapping
-    public void getAll(){
-
+    public List<Food> getAll(){
+        List<Food>foodList = repository.findAll();
+        return foodList;
     }
+
 }
