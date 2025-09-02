b48e23cc7cb341b81328a436000e7e730f720acf
helosantosdesousa <ssheloly@gmail.com>
2025-09-02 20:46:16 -0300

    Move backend to backend/ folder

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO foods (title, image, price) VALUES ('Refrigerante Lata', 'refri_lata.jpg', 5);
+INSERT INTO foods (title, image, price) VALUES ('Pizza Margherita', 'pizza_margherita.jpg', 20);
+INSERT INTO foods (title, image, price) VALUES ('Hamburguer Clássico', 'hamburguer_classico.jpg', 15);
+INSERT INTO foods (title, image, price) VALUES ('Salada Caesar', 'salada_caesar.jpg', 12);
+INSERT INTO foods (title, image, price) VALUES ('Suco Natural', 'suco_natural.jpg', 8);
+INSERT INTO foods (title, image, price) VALUES ('Pastel de Queijo', 'pastel_queijo.jpg', 7);
