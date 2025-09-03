356f750e60cf658a07af7089eb17d9e4740d69b3
Heloísa <115664843+helosantosdesousa@users.noreply.github.com>
2025-09-02 21:43:45 -0300

    Update import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO foods (title, image, price) VALUES ('Refrigerante Lata', 'refri_lata.jpg', 5);
-INSERT INTO foods (title, image, price) VALUES ('Pizza Margherita', 'pizza_margherita.jpg', 20);
-INSERT INTO foods (title, image, price) VALUES ('Hamburguer Clássico', 'hamburguer_classico.jpg', 15);
-INSERT INTO foods (title, image, price) VALUES ('Salada Caesar', 'salada_caesar.jpg', 12);
-INSERT INTO foods (title, image, price) VALUES ('Suco Natural', 'suco_natural.jpg', 8);
-INSERT INTO foods (title, image, price) VALUES ('Pastel de Queijo', 'pastel_queijo.jpg', 7);
+INSERT INTO foods (title, image, price) VALUES ('Refrigerante Lata', 'https://superprix.vteximg.com.br/arquivos/ids/205621/608165_1.jpg?v=637741485122270000', 5);
+INSERT INTO foods (title, image, price) VALUES ('Pizza Margherita', 'https://guiadacozinha.com.br/wp-content/uploads/2007/01/Pizza-marguerita-caseira.webp', 20);
+INSERT INTO foods (title, image, price) VALUES ('Hamburguer Clássico', 'https://www.estadao.com.br/resizer/v2/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg?quality=80&auth=a86d285f74ec7c08de7ba6ec10d557a463d905ffec2e56009d737687ac6054a1&width=1200&height=900&focal=553,494', 15);
+INSERT INTO foods (title, image, price) VALUES ('Salada Caesar', 'https://p2.trrsf.com/image/fget/cf/1200/900/middle/images.terra.com/2023/02/28/whatsapp-image-2023-02-28-at-01-53-47-(1)-1iyhprrq5e9tc.jpeg', 12);
+INSERT INTO foods (title, image, price) VALUES ('Suco Natural', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeC8r1jBik7TIXDDEbdD3aI4LFI85rxWVaIw&s', 8);
+INSERT INTO foods (title, image, price) VALUES ('Pastel de Queijo', 'https://www.sabornamesa.com.br/media/k2/items/cache/990810f9242641a8e264ce996a78ed28_XL.jpg', 7);
